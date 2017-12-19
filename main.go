077a8c2ed1863dac44e907ca44094101a9bc03d4
逆天邪皇 <1920829536@qq.com>
2017-12-19 10:40:20 +0800

    2017/12/19
    数据库初始化

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	_ "ReviewService/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/george518/PPGo_ApiAdmin/models"
 )
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+		models.Init()
 	}
 	beego.Run()
 }
---
+++ models/init.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+
+	maxIdle := 30
+	maxConn := 30
+
+	dbhost := beego.AppConfig.String("db.host")
+	dbport := beego.AppConfig.String("db.port")
+	dbuser := beego.AppConfig.String("db.user")
+	dbpassword := beego.AppConfig.String("db.password")
+	dbname := beego.AppConfig.String("db.name")
+
+	if dbport == "" {
+		dbport = "3306"
+	}
+
+	dns := dbuser + ":" + dbpassword + "@/tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8"
+
+	//设置最大空闲连接数和最大数据库连接
+	orm.SetMaxIdleConns("default", maxIdle)
+	orm.SetMaxOpenConns("default", maxConn)
+
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", dns)
+
+	//将用到的所有结构体映射到数据库中
+	orm.RegisterModel(new(User))
+
+	if beego.AppConfig.String("runmode") == beego.DEV {
+		orm.Debug = true
+	}
+}
+
+//该方法可以直接得到操作的哪张表名，因为统一处理，所以表名暂时定为service_user和service_examination
+func TableName(suffixName string) string {
+	return beego.AppConfig.String("db.prefix") + suffixName
+}
