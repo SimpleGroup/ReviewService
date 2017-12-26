7e9ad7735f504e62fa7a29254536016724fa248e
逆天邪皇 <1920829536@qq.com>
2017-12-26 19:09:41 +0800

    2017/12/26
    增加题目到DB未完成

--- controllers/question.go
+++ controllers/question.go
@@ -18,5 +18,18 @@ type QuestionController struct {
 // 将题目添加到数据库中
 func (q *QuestionController) AddQuestion() {
 	var question models.AllQuestion
-	json.Unmarshal(q.Ctx.Input.RequestBody, &question)
+	var resultData ResultData
+	bytes := q.Ctx.Input.RequestBody
+	println(string(bytes))
+	json.Unmarshal(bytes, &question)
+	isSuc := models.AddQuestionToDB(&question)
+	if isSuc {
+		resultData.Code=MSG_Suc
+		resultData.Msg="添加成功"
+	}else {
+		resultData.Code=MSG_ERR
+		resultData.Msg="添加失败"
+	}
+	q.Data["json"]=resultData
+	q.ServeJSON()
 }
--- models/question.go
+++ models/question.go
@@ -1,12 +1,14 @@
 package models
 
+import "github.com/astaxie/beego/orm"
+
 // 题目
 // Category:题目类别
 // Details:题目类别 0：安卓，1：java
 type AllQuestion struct {
 	Id       int              `orm:"pk"`
 	Category int              `orm:"column(category)"`
-	Details  *QuestionDetails `orm:"rel(one)"`
+	QuestionDetails  *QuestionDetails `orm:"rel(fk)"`
 }
 
 //自定义表名
@@ -20,14 +22,22 @@ func (q *AllQuestion) TableName() string {
 // Option:选择题的选项	选项形如 "A:...;B:...;C...;D...;"选项之间用分号隔开
 // Answer:选择题的答案
 type QuestionDetails struct {
-	Id           int    `orm:"pk"`
-	QuestionType int    `orm:"column(question_type)"`
-	Question     string `orm:"column(question)"`
-	Option       string `orm:"column(option)"`
-	Answer       string `orm:"column(answer)"`
+	Id           int          `orm:"pk"`
+	QuestionType int          `orm:"column(question_type)"`
+	Question     string       `orm:"column(question)"`
+	Option       string       `orm:"column(option)"`
+	Answer       string       `orm:"column(answer)"`
 }
 
-//自定义表名
-func (q *QuestionDetails) TableName() string {
-	return "service_question_details"
+////自定义表名
+//func (q *QuestionDetails) TableName() string {
+//	return "service_question_details"
+//}
+
+func AddQuestionToDB(q *AllQuestion) bool {
+	_, e := orm.NewOrm().Insert(q)
+	if e != nil {
+		return false
+	}
+	return true
 }
